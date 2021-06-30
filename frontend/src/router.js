ca6076e021a31a500c88578e692e80707582f2e2
Noex98 <johannes.knickering@hotmail.com>
2021-06-30 10:10:18 +0200

    transitions

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,7 @@ import Home from './views/Home.js'
 import About from './views/About.js'
 import Gallery from './views/Gallery.js'
 import Err404 from './views/Err404.js'
-import Transition from './utils/Transition.js'
+import {Transition__in, Transition__out} from './utils/Transition.js'
 
 const routes = [
     {
@@ -30,8 +30,19 @@ function render(hash){
     }
 }
 
+let transitionDelay = 300;
+
+// First render
 addEventListener('load', () => render(window.location.hash))
+
+// When navigating site
 addEventListener('hashchange', () => {
-    render(window.location.hash)
-    Transition()
+    // Transition out
+    Transition__out(transitionDelay)
+    // Wait for animation to finish
+    setTimeout(() => {
+        render(window.location.hash)
+        Transition__in(transitionDelay)
+    },transitionDelay)
+    
 })
--- frontend/src/utils/Transition.js
+++ frontend/src/utils/Transition.js
@@ -1,8 +1,15 @@
-export default function Transition(){
+export function Transition__in(delay){
     let root = document.getElementById('root');
-    root.style.animation = 'none'
+    root.style.animation = ''
     setTimeout(() => {
-        root.style.animation = 'fadeIn 750ms ease-in-out forwards'
+        root.style.animation = `fadeIn ${delay}ms ease-in-out forwards`
     }, 0)
-    
+}
+
+export function Transition__out(delay){
+    let root = document.getElementById('root');
+    root.style.animation = ''
+    setTimeout(() => {
+        root.style.animation = `fadeIn ${delay}ms ease-in-out reverse forwards`
+    }, 0)
 }
