6c03be64cd6ac31f09733714f038164f743b5a30
Noex98 <johannes.knickering@hotmail.com>
2021-07-26 13:22:06 +0200

    .

--- frontend/src/utils/Link.js
+++ frontend/src/utils/Link.js
@@ -1,9 +1,7 @@
 export default function Link(path, content){
 
     return (
-        `<a href="javascript:;" 
-                    onclick="history.pushState('${path}', null, '${path}'); 
-                    dispatchEvent(new Event('navigate', { bubbles: true, cancelable: false }))">
+        `<a onclick="event.preventDefault(); location.hash = '${path}'">
             ${content}
         </a>`
     )
