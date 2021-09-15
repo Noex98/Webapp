aff49d9cd23621a1c6dd609bdc39159b75c88ed4
Johannes Knickering <73876847+Noex98@users.noreply.github.com>
2021-09-15 15:03:12 +0200

    .

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,12 +1,15 @@
 import Header from '../components/Header.js'
+import Nav from '../components/Nav.js'
 import { user } from '../Store.js'
 
 export default function Home(){
 
-    let user = user.data()
+    let userData = user.data()
+    console.log(userData)
 
     return (/*html*/ `
             ${Header()}
             <h1>Home component</h1>
+            ${Nav()}
     `)
 }
